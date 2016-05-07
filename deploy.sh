a49316de856b7d3b44ca5865c7a2fda4c70fb96e
Oleksandr Shturmov <oleks@oleks.info>
2016-05-07 11:05:29 +0200

    Explain -B

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,15 @@
 
 set -euo pipefail
 
+# Use -B to keep gh-pages at most one commit ahead of master.
+# This leads to the use of -f when pushing to gh-pages on remote.
+# This might actually be one descent use of git push -f..
 git checkout -B gh-pages
 
 set +e
 cat | bash - <<EOF
 set -euo pipefail
 
-git merge master
 emacs index.org --batch -f org-html-export-to-html --kill
 git add -f index.html
 git commit -S -m 'Deploy'
